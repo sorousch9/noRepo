7431997b5b3061f001e2e1dcb59314a3d657da64
Soroush <soroush_raw@hotmail.com>
2022-08-09 14:49:49 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Container } from "react-bootstrap";
-import Home from "./Pages/Home"
+import Home from "./Pages/Home";
 const App = () => {
   return (
-    <div>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Home/>
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <Fragment>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} exact />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Fragment>
+    </Router>
   );
 };
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,8 +5,16 @@ const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <a href={`./product/${product._id}`}>
-        <Card.Img src={product.image} variant="top"/>
+        <Card.Img src={product.image} variant="top" />
       </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">{product.name}</Card.Title>
+        </a>
+      </Card.Body>
+
+      <Card.Text as="h3">{product.price}</Card.Text>
     </Card>
   );
 };
