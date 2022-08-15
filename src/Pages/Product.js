f819942f0967944b62338dc832471ae5e4036f55
Soroush <soroush_raw@hotmail.com>
2022-08-15 16:07:47 +0200

    product page updated

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -5,9 +5,38 @@ import products from "../products";
 
 const Product = () => {
   const { id } = useParams();
-  const product = products.find((item) => item._id === (id));
+  const product = products.find((item) => item._id === id);
 
-  return <div>{product.name}</div>;
+  return (
+    <div>
+      <Link to="/" className="btn btn-light my-3">
+        Zurück
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} fliud="true" />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>{product.price}</ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Button className="btn-block" type="button">
+                In den Warenkorb
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Product;
